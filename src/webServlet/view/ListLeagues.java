9c4e3c99893c333e1a378333406bb987a11c94b9
root <root@localhost.localdomain>
2017-09-19 18:14:19 +0300

    add ant-emacs 02 to master

---
+++ src/webServlet/view/ListLeagues.java
@@ -0,0 +1,50 @@
+package webServlet.view;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import webServlet.model.League;
+
+/**
+ * Describe class ListLeagues here.
+ *
+ *
+ * Created: Fri Sep 15 12:06:41 2017
+ *
+ * @author <a href="mailto:root@localhost.localdomain">root</a>
+ * @version 1.0
+ */
+public class ListLeagues extends HttpServlet {
+
+    /**
+     * Describe <code>doGet</code> method here.
+     *
+     * @param httpServletRequest a <code>HttpServletRequest</code> value
+     * @param httpServletResponse a <code>HttpServletResponse</code> value
+     * @exception ServletException if an error occurs
+     * @exception IOException if an error occurs
+     */
+    public final void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+	ServletContext context = getServletContext();	
+	ArrayList<League> leagueList = (ArrayList<League>)context.getAttribute("leagueList");
+	response.setContentType("text/html");
+	PrintWriter out = response.getWriter();
+	if (leagueList==null) {
+	    String leagueFile =(String) context.getAttribute("leagueFile");
+	    out.println("leagueFile = "+ leagueFile +" <br/>");
+	    out.println("cant load list leagues from league file");
+	} else {
+	    for (League league : leagueList) {
+		out.println("year "+league.getYear()+" <br/>");
+		out.println("title "+league.getSeason()+" <br/>");
+		out.println("season "+league.getTitle()+" <br/>");
+		out.println("===================  <br/>");
+	    }
+	}
+    }
+}
